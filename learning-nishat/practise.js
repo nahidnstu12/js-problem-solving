b0e1722ffa48bb276f618a5eb72e682497040c10
nahidnstu12 <nahid.dev19@gmail.com>
2022-05-11 11:30:28 +0600

    array problems

---
+++ learning-nishat/practise.js
@@ -0,0 +1,11 @@
+let arr = 1;
+let change = (val) => {
+//  val = 3 //assignemnt  // 1
+ val = val + 3 // 1
+};
+change(arr) //passed by refernce
+console.log(arr); // 1
+// let a = arr;
+// arr.push(6)
+// console.log({arr,a});
+// console.log(output.replaceAll(","," "));
